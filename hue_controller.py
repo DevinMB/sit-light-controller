96c6ed9fbf3618ec7257bee5c8ae54a6dad9abdd
Devin Butts <devinbutts@Devins-MacBook-Pro.local>
2024-02-17 11:24:58 -0500

    fix env load issue

--- hue_controller.py
+++ hue_controller.py
@@ -1,12 +1,23 @@
 import requests
 import os
+import json
 from dotenv import load_dotenv
-load_dotenv()
+
 
 class HueController:
+    
     def __init__(self, bridge_ip, user_token):
+        load_dotenv()  # Ensure environment variables are loaded
         self.bridge_ip = bridge_ip
         self.user_token = user_token
+        # Parse environment variables after ensuring they're loaded
+        try:
+            self.off_config = json.loads(os.getenv('LIGHT_OFF_CONFIG'))
+            self.on_config = json.loads(os.getenv('LIGHT_ON_CONFIG'))
+        except json.JSONDecodeError:
+            print("Error decoding JSON from environment variables")
+            self.off_config = {}
+            self.on_config = {}
 
     def make_api_call_to_group(self, group_id, data):
         url = f'http://{self.bridge_ip}/api/{self.user_token}/groups/{group_id}/action'
@@ -19,10 +30,12 @@ class HueController:
         return response.json()
 
     def turn_on_light(self, light_id):
-        data = os.getenv('LIGHT_ON_CONFIG')
+        print(os.getenv('LIGHT_ON_CONFIG'))
+        data = self.on_config
         return self.make_api_call_to_light(light_id, data)
 
     def turn_off_light(self, light_id):
-        data = os.getenv('LIGHT_OFF_CONFIG')
+        print(os.getenv('LIGHT_OFF_CONFIG'))
+        data = self.off_config
         return self.make_api_call_to_light(light_id, data)
 
