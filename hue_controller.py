58920fda1cafba26a6978e4d2cde14238786edc0
Devin Butts <devinbutts@Devins-MBP.localdomain>
2025-04-15 17:11:45 -0400

    oops

--- hue_controller.py
+++ hue_controller.py
@@ -34,9 +34,9 @@ class HueController:
         return response.json()
 
     def turn_on_light(self):
-        self.set_previous_light_config()
         data = self.on_config
         if(self.control_type == 'LIGHT'):
+            self.set_previous_light_config()
             return self.make_api_call_to_light(data)
         if(self.control_type == 'GROUP'):
             return self.make_api_call_to_group(data)
@@ -50,7 +50,8 @@ class HueController:
     
     def set_previous_light_config(self):
         url = f'http://{self.bridge_ip}/api/{self.user_token}/lights/{self.light_id}'
-        response = requests.get(url)
+
+        response = requests.get(url).json()
         previous_state = {
             "on": response["state"]["on"],
             "bri": response["state"]["bri"],
